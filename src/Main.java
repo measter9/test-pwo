693d6c55a9b477d12fb1e10dd6d1a468aacba043
pavilion <jakisfajnyktos@gmail.com>
2024-10-17 18:26:53 +0200

    test2 commit

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,9 @@ public class Main {
         System.out.printf("Hello and welcome!");
         System.out.printf("Hello and welcome!");
         System.out.printf("Hello and welcome!");
+        System.out.printf("Hello and welcome!");
+        System.out.printf("Hello and welcome!");
+
 
     }
 }
