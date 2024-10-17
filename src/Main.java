96cec4a7e2ccb67751857d9701d49938609bae86
pavilion <jakisfajnyktos@gmail.com>
2024-10-17 18:30:19 +0200

    feat: logowanie

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,12 @@ public class Main {
         System.out.printf("Hello and welcome!");
         System.out.printf("Hello and welcome!");
         System.out.printf("Hello and welcome!");
+        System.out.printf("Hello and welcome!");
+        System.out.printf("Hello and welcome!");
+        System.out.printf("Hello and welcome!");
+        System.out.printf("Hello and welcome!");
+        System.out.printf("Hello and welcome!");
+        System.out.printf("Hello and welcome!");
 
 
     }
